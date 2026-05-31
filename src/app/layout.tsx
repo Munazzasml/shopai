163fff8dcc4093ea01aed65e6ee9b7f72e0faccd
munazza ismail <munazzaismail@3gmail.com>
2026-05-31 17:36:53 +0500

    Disable chatbot for frontend-only deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           </main>
           
           <Footer />
-          <ChatWidget />
+          {/* <ChatWidget /> */}
         </CartProvider>
       </body>
     </html>
